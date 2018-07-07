28a03161173ea8a59fbd6a4a4490d8cd6ef7a105
veeshoo@gmail.com <veeshoo@gmail.com>
2018-07-07 20:57:39 +0530

    Heroku Enabled

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-//const chalk = require('chalk');
-//const debug = require('debug')('app');
+
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -48,6 +48,6 @@ app.get('/about', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000.")
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 });
